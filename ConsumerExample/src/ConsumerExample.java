3f43c79e712458407efb9fcc5a33ec58ca5170eb
minu youn <anomie7777@gmail.com>
2017-07-16 21:34:20 +0900

    백업] Chapter.14 Lambda Expressions p694 20170716

---
+++ ConsumerExample/src/ConsumerExample.java
@@ -0,0 +1,21 @@
+import java.util.function.Consumer;
+import java.util.function.DoubleConsumer;
+import java.util.function.ObjIntConsumer;
+import java.util.function.BiConsumer;
+
+public class ConsumerExample {
+	public static void main(String[] args) {
+		Consumer<String> consumer = t -> System.out.println(t + 8);
+		consumer.accept("java");
+		
+		BiConsumer<String, String> bigConsumer = (t, u) -> System.out.println(t + u);
+		bigConsumer.accept("java", "8");
+		
+		DoubleConsumer doubleConsumer = d -> System.out.println("java" + d);
+		doubleConsumer.accept(8.0);
+		
+		ObjIntConsumer<String> objIntConsumer = (t, i) -> System.out.println(t + i);
+		objIntConsumer.accept("ÀÚ¹Ù", 8);
+	}
+
+}
