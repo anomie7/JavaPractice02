0594c66c6614a5c295017264c020a69a367a3d19
minu youn <anomie7777@gmail.com>
2017-07-19 18:23:41 +0900

    백업] Chapter.14 Lambda Expressions 완료 p716 20170719

---
+++ OperatorMinByMaxByExample/src/OperatorMinByMaxByExample.java
@@ -0,0 +1,17 @@
+import java.util.function.BinaryOperator;
+
+public class OperatorMinByMaxByExample {
+	public static void main(String[] args) {
+		BinaryOperator<Fruit> binaryOperator;
+		Fruit fruit;
+
+		binaryOperator = BinaryOperator.minBy((f1, f2) -> Integer.compare(f1.getPrice(), f2.getPrice()));
+		fruit = binaryOperator.apply(new Fruit("µþ±â", 6000), new Fruit("¼ö¹Ú", 10000));
+		System.out.println(fruit.getName());
+
+		binaryOperator = BinaryOperator.maxBy((f1, f2) -> Integer.compare(f1.getPrice(), f2.getPrice()));
+		fruit = binaryOperator.apply(new Fruit("µþ±â", 6000), new Fruit("¼ö¹Ú", 10000));
+		System.out.println(fruit.getName());
+	}
+
+}
